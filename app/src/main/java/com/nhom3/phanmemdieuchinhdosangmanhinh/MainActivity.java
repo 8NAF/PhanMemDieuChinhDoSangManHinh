9e287f75f380410ff20da33ff38c07bef4ea5cab
Mai Anh Khoa <maikhoa000@gmail.com>
2020-11-06 14:45:40 +0700

    hihihi

--- app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/MainActivity.java
+++ app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		String a = "3";
+		String a = "6";
 	}
 }
