4a438f5b2b50a9669ccdb1dcfc1dbcbde8e3cf15
Mai Anh Khoa <maikhoa000@gmail.com>
2020-11-06 13:55:22 +0700

    Initial commit

---
+++ app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nhom3.phanmemdieuchinhdosangmanhinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
