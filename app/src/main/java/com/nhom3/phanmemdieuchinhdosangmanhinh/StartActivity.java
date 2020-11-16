43682526c1f36b6856ef71bc70bc61226455032f
Mai Anh Khoa <maikhoa000@gmail.com>
2020-11-16 12:47:34 +0700

    Đổi tên HomeActivity.java thành StartActivity.java Đổi tên activity_home.xml thành activity_start.xml

--- app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/StartActivity.java
+++ app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/StartActivity.java
@@ -6,20 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 import com.google.android.material.tabs.TabLayout;
 
-public class HomeActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 	private TabLayout tabLayout;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		setContentView(R.layout.activity_start);
 
 		ImageButton btnLogin;
 		btnLogin = (ImageButton) findViewById(R.id.imb_power);
@@ -27,7 +25,7 @@ public class HomeActivity extends AppCompatActivity {
 		btnLogin.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+				Intent intent = new Intent(StartActivity.this, MainActivity.class);
 				startActivity(intent);
 			}
 		});
