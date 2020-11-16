6dbb9a403198f77bdeca6a9549a1c2218e5dc7db
Mai Anh Khoa <maikhoa000@gmail.com>
2020-11-16 19:46:17 +0700

    Xóa các activity cũ để sửa lại giao diện

--- app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/MainActivity.java
+++ app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/MainActivity.java
@@ -11,13 +11,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     //region Attributes
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+
 
     //endregion
     //region Override Methods
@@ -29,21 +29,14 @@ public class MainActivity extends AppCompatActivity {
 
         this.mapped();
 
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(
-            this.getSupportFragmentManager(),
-            FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
-        );
 
-        this.viewPager.setAdapter(viewPagerAdapter);
-        this.tabLayout.setupWithViewPager(viewPager);
     }
 
     //endregion
     //region Helper Methods
 
     protected void mapped() {
-        this.tabLayout = (TabLayout) this.findViewById(R.id.tab_layout);
-        this.viewPager = (ViewPager) this.findViewById(R.id.view_pager);
+
     }
 
     //endregion
