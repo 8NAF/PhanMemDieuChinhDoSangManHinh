34300b4ce9da39480efea9eb5cb58dcb754bdc5e
Mai Anh Khoa <maikhoa000@gmail.com>
2020-11-16 22:36:47 +0700

    Kết hợp navigation drawer và tab layout

--- app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/MainActivity.java
+++ app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/MainActivity.java
@@ -1,44 +1,55 @@
 package com.nhom3.phanmemdieuchinhdosangmanhinh;
 
 import android.os.Bundle;
-import android.text.NoCopySpan;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.tabs.TabItem;
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    //region Attributes
+	//region Attributes
 
+	DrawerLayout dwlMain;
+	ActionBarDrawerToggle toggle;
+	NavigationView ngvMain;
+	Toolbar tlbMain;
 
+	//endregion
+	//region Override Methods
 
-    //endregion
-    //region Override Methods
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+		this.mapped();
 
-        this.mapped();
+		this.setSupportActionBar(this.tlbMain);
+		this.toggle = new ActionBarDrawerToggle(
+			this,
+			this.dwlMain,
+			this.tlbMain,
+			R.string.open,
+			R.string.close
+		);
+		this.dwlMain.addDrawerListener(toggle);
+		this.toggle.setDrawerIndicatorEnabled(true);
+		this.toggle.syncState();
+		this.toggle.getDrawerArrowDrawable().setColor(this.getColor(R.color.white));
+	}
 
+	//endregion
+	//region Helper Methods
 
-    }
+	protected void mapped() {
+		this.tlbMain = (Toolbar) this.findViewById(R.id.tlb_main);
+		this.dwlMain = (DrawerLayout) this.findViewById(R.id.dwl_main);
+		this.ngvMain = (NavigationView) this.findViewById(R.id.ngv_main);
+	}
 
-    //endregion
-    //region Helper Methods
-
-    protected void mapped() {
-
-    }
-
-    //endregion
+	//endregion
 }
 
