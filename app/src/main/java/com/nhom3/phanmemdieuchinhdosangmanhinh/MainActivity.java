4e1fe4b94fe29fff0f3b41a8e51df108ba021784
Mai Anh Khoa <maikhoa000@gmail.com>
2020-11-19 14:48:10 +0700

    Lần đầu tiên đổi màu được màn hình

--- app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/MainActivity.java
+++ app/src/main/java/com/nhom3/phanmemdieuchinhdosangmanhinh/MainActivity.java
@@ -1,10 +1,16 @@
 package com.nhom3.phanmemdieuchinhdosangmanhinh;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.Settings;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -37,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
 	SeekBar skbGreen;
 	SeekBar skbBlue;
 
+	private SharedMemory mSharedMemory;
+	private CountDownTimer mCountDownTimer;
+
 	//endregion
 	//region Override Methods
 
@@ -49,12 +58,85 @@ public class MainActivity extends AppCompatActivity {
 		this.setSupportActionBar(this.tlbMain);
 		this.ngvMain.setItemIconTintList(null); //Đặt null để có thể thêm icon cho các menu item
 		this.addOrSetListener();
+
+		mSharedMemory = new SharedMemory(this);
+
+		SeekBar.OnSeekBarChangeListener changeListener = new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				mSharedMemory.setAlpha(skbAlpha.getProgress());
+				mSharedMemory.setRed(skbRed.getProgress());
+				mSharedMemory.setGreen(skbGreen.getProgress());
+				mSharedMemory.setBlue(skbBlue.getProgress());
+
+				if (ScreenFilterService.STATE == ScreenFilterService.STATE_ACTIVE) {
+					Intent i =new Intent(MainActivity.this, ScreenFilterService.class);
+					startService(i);
+				}
+
+				swtOnOff.setChecked(ScreenFilterService.STATE == ScreenFilterService.STATE_ACTIVE);
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+
+			}
+		};
+
+		skbAlpha.setOnSeekBarChangeListener(changeListener);
+		skbRed.setOnSeekBarChangeListener(changeListener);
+		skbGreen.setOnSeekBarChangeListener(changeListener);
+		skbBlue.setOnSeekBarChangeListener(changeListener);
+
+		swtOnOff.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i =new Intent(MainActivity.this, ScreenFilterService.class);
+				if (ScreenFilterService.STATE == ScreenFilterService.STATE_ACTIVE) {
+					stopService(i);
+				} else {
+					if(Build.VERSION.SDK_INT >= 23) {
+						if (!Settings.canDrawOverlays(MainActivity.this)) {
+							Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+									Uri.parse("package:" + getPackageName()));
+							startActivityForResult(intent, 1234);
+						} else {
+							startService(i);
+						}
+					}
+					else
+					{
+						startService(i);
+					}
+				}
+
+				refresh();
+			}
+		});
+
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 		this.tloMain.getTabAt(0).select();
+		swtOnOff.setChecked(ScreenFilterService.STATE == ScreenFilterService.STATE_ACTIVE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == 1234) {
+			if (resultCode == RESULT_OK) {
+				Intent i =new Intent(MainActivity.this, ScreenFilterService.class);
+				startService(i);
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 
 	//endregion
@@ -76,7 +158,6 @@ public class MainActivity extends AppCompatActivity {
 		this.skbGreen = this.findViewById(R.id.skbGreen);
 		this.skbBlue = this.findViewById(R.id.skbBlue);
 
-
 	}
 
 	void addOrSetListener() {
@@ -84,7 +165,25 @@ public class MainActivity extends AppCompatActivity {
 		this.dwlMain.addDrawerListener(new dwlMain_DrawerListener());
 		this.tloMain.addOnTabSelectedListener(new tloMain_OnTabSelectedListener());
 
+	}
 
+	private void refresh() {
+		if (mCountDownTimer != null)
+			mCountDownTimer.cancel();
+
+		mCountDownTimer = new CountDownTimer(100, 100) {
+			@Override
+			public void onTick(long millisUntilFinished) {
+
+			}
+
+			@Override
+			public void onFinish() {
+				swtOnOff.setChecked(ScreenFilterService.STATE == ScreenFilterService.STATE_ACTIVE);
+			}
+		};
+
+		mCountDownTimer.start();
 	}
 
 	//endregion
